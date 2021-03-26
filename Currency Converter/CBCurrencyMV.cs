a7d6861dfdc2380326fa63cce6893c8b89b3101d
ilhambayhaqi <ilham.bhq28@gmail.com>
2021-03-26 22:28:29 +0700

    Add NET Project

---
+++ Currency Converter/CBCurrencyMV.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Converter
+{
+    class CBCurrencyMV
+    {
+        private string jsonfile = "CurrencyNames.json";
+        private string jsonapi = "CurrencyExchange.json";
+        private string _filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\CurrencyConverterApp\";
+        private string apiBase = "http://data.fixer.io/";
+        private string apiKey = "7dd299e649864228fb2f5d07da90e14c";
+        private JObject currencyName, currencyExchange;
+
+        public Dictionary<string, KeyValuePair<double, string>> CurrencyCollection { get; set; }
+        public CBCurrencyMV()
+        {
+            CurrencyCollection = new();            
+            deserialiseJSON(ref currencyName, _filePath + jsonfile);
+            requestAPIData();
+            //deserialiseJSON(ref currencyExchange, _filePath + jsonapi);
+
+            foreach(JProperty rates in ((JObject) currencyExchange["rates"]).Properties() ){
+                string ratesName = (rates.Name).ToString();
+                double ratesExchange = double.Parse(rates.Value.ToString());
+                string ratesAlias = currencyName[ratesName]["currencyName"].ToString();
+                CurrencyCollection.Add(ratesName, new(ratesExchange, ratesAlias));
+            }
+        }
+
+        private void deserialiseJSON(ref JObject output, string fileJSON)
+        {
+            using (StreamReader file = File.OpenText(fileJSON))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                output = (JObject)JToken.ReadFrom(reader);
+            }
+        }
+
+        private void requestAPIData()
+        {
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(apiBase);
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("api/latest?access_key=" + apiKey).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                using (StreamWriter sw = new StreamWriter(_filePath + jsonapi))
+                {
+                    sw.WriteLine(result);
+                }
+                deserialiseJSON(ref currencyExchange, _filePath + jsonapi);
+            }
+        }
+
+        public JObject getCurrencyName()
+        {
+            return currencyName;
+        }
+
+        public JObject getCurrencyExchange()
+        {
+            return currencyExchange;
+        }
+    }
+}
---
+++ Currency Converter/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Currency_Converter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+
+    public partial class MainWindow : Window
+    {
+        private bool cbFrom_USelected = false;
+        private bool cbTo_USelected = false;
+        private JObject currencyExchange;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CBCurrencyMV cBCurrencyMV = new CBCurrencyMV();
+            cbFrom.DataContext = cBCurrencyMV;
+            cbTo.DataContext = cBCurrencyMV;
+            currencyExchange = cBCurrencyMV.getCurrencyExchange();
+            tbDate.Text = "Last Update: " + currencyExchange["date"].ToString();
+            initCurrency();
+        }
+        private void initCurrency()
+        {
+            tbFrom.Text = "0";
+            
+            cbFrom.SelectedIndex = 0;
+            cbTo.SelectedIndex = 1;
+        }
+
+        private void clearCurrency()
+        {
+            tbFrom.Text = "";
+            tbTo.Text = "";
+        }
+
+
+        private void TbFrom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!double.TryParse(tbFrom.Text, out _))
+            {
+                tbFrom.Text = "";
+            }
+            else if (tbFrom.IsFocused)
+            {
+                calcCurrency(tbFrom, tbTo, cbFrom, cbTo);
+            }
+        }
+
+        private void TbTo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!double.TryParse(tbTo.Text, out _) && tbTo.IsFocused)
+            {
+                tbTo.Text = "";
+            }
+            else if (tbTo.IsFocused)
+            {
+                calcCurrency(tbTo, tbFrom, cbTo, cbFrom);
+            }
+
+        }
+
+        private void BtnSwapCurrency_Click(object sender, RoutedEventArgs e)
+        {
+            calcCurrency(tbFrom, tbTo, cbFrom, cbTo);
+            int fromCurrency = cbFrom.SelectedIndex;
+            cbFrom.SelectedIndex = cbTo.SelectedIndex;
+            cbTo.SelectedIndex = fromCurrency;
+
+            calcCurrency(tbFrom, tbTo, cbFrom, cbTo);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            clearCurrency();
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            calcCurrency(tbFrom, tbTo, cbFrom, cbTo);
+        }
+
+        private void calcCurrency(TextBox fromText, TextBox toText, ComboBox fromCombo, ComboBox toCombo)
+        {
+            if (!string.IsNullOrEmpty(fromText.Text))
+            {
+                double fromCurrency = ((KeyValuePair<string, KeyValuePair<double, string>>)fromCombo.SelectedItem).Value.Key;
+                double toCurrency = ((KeyValuePair<string, KeyValuePair<double, string>>)toCombo.SelectedItem).Value.Key;
+                double result = double.Parse(fromText.Text) * toCurrency / fromCurrency;
+
+                toText.Text = result.ToString("F");
+                tbFromRes.Text = tbFrom.Text + " " + ((KeyValuePair<string, KeyValuePair<double, string>>)cbFrom.SelectedValue).Value.Value + " equal";
+                tbToRes.Text = tbTo.Text + " " + ((KeyValuePair<string, KeyValuePair<double, string>>)cbTo.SelectedValue).Value.Value;
+            }
+        }
+
+
+        private void cbFrom_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbFrom_USelected)
+            {
+                calcCurrency(tbFrom, tbTo, cbFrom, cbTo);
+                cbFrom_USelected = false;
+            }
+        }
+
+        private void cbTo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbTo_USelected)
+            {
+                calcCurrency(tbFrom, tbTo, cbFrom, cbTo);
+                cbFrom_USelected = false;
+            }
+        }
+
+        private void cbFrom_DropDownOpened(object sender, EventArgs e)
+        {
+            cbFrom_USelected = true;
+        }
+
+        private void cbTo_DropDownOpened(object sender, EventArgs e)
+        {
+            cbTo_USelected = true;
+        }
+    }
+}
